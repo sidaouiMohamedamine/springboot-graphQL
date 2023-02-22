2704846bf29ee6ba9a652072fb992b64c32a603d
Sidaoui <mssidaoui@gmail.com>
2023-02-22 23:20:07 +0100

    project skeleton

---
+++ GraphQL_Project/src/main/java/com/example/graphql_project/GraphQlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphql_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlProjectApplication.class, args);
+    }
+
+}
---
+++ GraphQL_Project/src/test/java/com/example/graphql_project/GraphQlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphql_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
